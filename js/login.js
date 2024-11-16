b619608948337a84cf0965c01ee3ec1e0aa0bd56
Paula Castano <paula.castano1@utp.edu.co>
2024-11-15 23:46:18 -0500

    login integrado

--- js/login.js
+++ js/login.js
@@ -1,6 +1,12 @@
 // Iniciar sesión y cambiar pantalla
 document.getElementById("login-form").addEventListener("submit", function (event) {
     event.preventDefault();
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
     document.querySelector(".login-container").style.display = "none";
-    document.location.assign('./index.html')
+    if (username == 'admin' && password == '123') {
+        document.location.assign('./main.html')
+    } else {
+        document.location.assign('./index.html')
+    }
 });
